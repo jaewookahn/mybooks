c57193c89610068d7f12ecc64ac665bd48d0a30b
Jae-wook Ahn <jaewook.ahn@us.ibm.com>
2020-05-30 23:09:51 -0400

    Add data grid (react-data-grid v7 does not work)

--- src/App.js
+++ src/App.js
@@ -2,24 +2,55 @@ import React from 'react';
 import './App.css';
 import DataReader from "./DataReader";
 import Papa from "papaparse"
+import ReactDataGrid from 'react-data-grid'
 
-function App() {
+const columns = [
+  { key: "a", name: "Column A", editable: true, width: 200, resizable: true },
+  { key: "b", name: "Column B", editable: true, width: 250, resizable: true  },
+];
 
-  var reader = new DataReader('http://localhost:3000')
-  reader.getCSVData('test.csv').then((response) => {
-    var data = Papa.parse(response)
-  })
-  .catch((e) => {
-      console.log('Error', e)
-  })
+class App extends React.Component {
+
+  state = { rows: [] }
+
+  constructor() {
+    
+    super()
+
+    var reader = new DataReader('http://localhost:3000')
+    reader.getCSVData('test.csv').then((response) => {
+      var data = Papa.parse(response)
+      var rows = []
+
+      data.data.forEach(r => {
+        rows.push({'a':r[0], 'b':r[1]})
+      })
+
+      this.state.rows = rows
+    })
+    .catch((e) => {
+        console.log('Error', e)
+    })  
+  }
+
+  render() {
+    return (
+
+      <div className="App">
+        <header className="App-header">
+          MyBooks
+        </header>
   
-  return (
-    <div className="App">
-      <header className="App-header">
-        MyBooks
-      </header>
-    </div>
-  );
+        <ReactDataGrid
+            columns={columns}
+            rowGetter={i => this.state.rows[i] }
+            rowsCount={20}
+            onGridRowsUpdated={this.onGridRowsUpdated}
+            enableCellSelect={true}
+        />
+      </div>
+    );  
+  }
 }
 
 export default App;
